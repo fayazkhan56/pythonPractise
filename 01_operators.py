4e4d0cda12f839c35b5f5fb67394df7b7e9fd8af
fayazkhan56 <fayazkhansultan@gmail.com>
2023-01-01 23:31:48 +0500

    my first commit

---
+++ 01_operators.py
@@ -0,0 +1,10 @@
+# print(2+3)
+# print(4-2)
+# print(2*6)
+# print(12/2)
+# print(13%3)
+# print(12//2)
+#print(2**3)
+
+#PEMDAS rule paranthesis exponent Mul Div Ad Sub or LEft to RIght
+#print(2+6*3/2-3**6)
